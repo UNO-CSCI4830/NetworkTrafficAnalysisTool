5e04425911ce473076ee145c24552ecc23f28942
danielmaher4277 <131501940+danielmaher4277@users.noreply.github.com>
2026-04-07 14:30:51 -0500

    For FR19 Historical logs

---
+++ connectionHistory.py
@@ -0,0 +1,67 @@
+import datetime
+import main
+
+
+
+todaysDate = datetime.datetime.today
+
+storingPeriod = datetime.timedelta.days(30)
+
+expirationDate = todaysDate - storingPeriod
+
+connectionsArray = []
+
+ID = 0
+
+#This should be run frequently to keep stored list current
+def getConnections():
+
+    connections = main.connections
+
+    formattedConnection = [connection, todaysDate, ID++]
+
+    for connection in connections:
+        connectionsArray.append(formattedConnection)
+    
+
+
+#This can be run on the order of once a day
+def checkExpiration(date):
+    for connection in connectionsArray:
+        if connection[1] < expirationDate:
+            clearSpecificConnection(connection[2])
+        
+    
+
+
+#This can be run on the order of once a day
+#This throws an error and alerts the user if the size of connections array exceeds 10,000 connections. This is a large number of connections, but still a relitively small amount of storage space. Since each individual connection is only a few bytes of data, storing this # will not be a problem. If left to run unchecked, it could(potentially but not likely) use a ridiculous amount of data while storing an unnecesary number of past connections. 
+def checkSizeConnectionsArray(connectionsArray):
+    if len(connectionsArray) > 10000:
+        raise Exception("Array has exceeded maximum number of connections and an action must be taken.")
+    
+    else:
+        print(f"Array is storing a suitable number of connections, currently storing the last {len(connectionsArray)} connections from the last 30 days.")
+    
+
+
+
+def fullyClearConnectionArray():
+    connectionsArray = []
+
+
+#Takes in a certain number as a parameter and removes the oldest connections up to that number of connections.
+def clearOldestConnections(numConnections = 1000):
+    for number in numConnections:
+        connectionsArray.pop()
+    
+
+
+#Removes specific connection log based off of its ID
+def clearSpecificConnection(ID):
+    for connection in connectionsArray:
+        if connection[2] == ID:
+            connectionsArray.remove(connection)
+        
+    
+
